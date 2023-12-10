e45a131fe3c159713a342e365b65c0a7e7defeff
PhongMX <phongmx2021>
2023-12-10 19:31:08 +0700

    fuck

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -30,8 +30,8 @@ def login(user, password):
     return driver
 
 def add_cart(driver, n_items):
-    products = driver.find_elements_by_css_selector("div.inventory_item_name ")
-    print(timestamp() + 'Get all name'+ products)
+    products = driver.find_elements_by_css_selector("div.inventory_item_name ").text
+    print(timestamp() + 'Get all name'+ len(products))
     print(timestamp() + 'name 1'+ products[0])
 
     for i in range(n_items):
